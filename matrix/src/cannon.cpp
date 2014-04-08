886a1b9c23b61e1571d2148c278f6148d7829294
JordanBlocher <jordanblocher@gmail.com>
2014-04-07 19:20:31 -0700

    asdf

--- matrix/src/cannon.cpp
+++ matrix/src/cannon.cpp
@@ -28,10 +28,6 @@
 MPI_Datatype MPI_Vector;
 
 template <typename T>
-void master(Matrix<T>&, Matrix<T>&, Matrix<T>&);
-template <typename T>
-void slave(Matrix<T>&, Matrix<T>&, Matrix<T>&);
-template <typename T>
 void work(T*,T*,T*);
 
 int N, numprocs, size, size2;
@@ -63,92 +59,92 @@ int main (int argc, char *argv[]) {
 
     size = N / (int)sqrt(numprocs);
     size2 = size*size;
-    Matrix<int> B(N, N);
-    Matrix<int> A(N, N);
-    Matrix<int> C(N, N);
-    C.clear();
- 
+    Matrix<int> *Ap, *Bp, *Cp;
+    int *Arow, *Brow, *Crow;
+    Arow = new int[size2];
+    Brow = new int[size2];
+    Crow = new int[size2];
+
     if(rank == 0)
     {
-       // Initialize matrix A random, B identity
+        Ap = new Matrix<int>(N,N);
+        Bp = new Matrix<int>(N,N);
+        Cp = new Matrix<int>(N,N);
+        Cp->clear();
+
+        Matrix<int> &A = *Ap;
+        Matrix<int> &B = *Bp;
+
+        // Initialize matrix A random, B identity
         for(i=0; i<N; i++)
         {
             for(j=0; j<N; j++)
             {
-                A(i, j) = rand() % 20 + 1;
-                C(i, j) = 0;
+                A(i, j) = i*N+j;
                 if(i==j)
-                    B(i, j) = 1;
-                else B(i, j) = 0;
-                printf("\t(%d,%d) ", i, j);
+                    B(N-i-1, j) = 1;
+                else B(N-i-1, j) = 0;
             }
-            printf("\n");
         }
-        B(0, 1) = 2;
-        B(0, 2) = 3;
-        std::cout<<"A\n";
-        matprintf(A);
-        std::cout<<"B\n";
-        matprintf(B);
-        std::cout<<"\n\n\n";
-        //k=0;
-        /*for(i=0; i<=N; i+=size)
-            for(j=0; j<N; j+=size)
-            {
-                A.submat(i, j, size, k);
-                submatprintf(A.smat[k], size, size, "Amaster");
-                B.submat(i, j, size, k);
-                submatprintf(B.smat[k], size, size, "Bmaster");
-                C.submat(i, j, size, k);
-                k++;
-            }
-*/
+        A.setRowIndexing(size, N, true);
+        B.setRowIndexing(size, N, true);
+
+        pmatprintf(A, size, "A");
+        pmatprintf(B, size, "B");
+        
         startclock = std::chrono::high_resolution_clock::now();
-       
+
+        A.shift(size, N, false, true);
+        B.shift(size, N, true, true);
     }
-    A.partition(size, N, rank);
-    B.partition(size, N, rank, true);
-    if(rank == 0)
-    {
-    printf("A partitioned\n");
-    matprintf(A);
-    printf("B partitioned\n");
-    matprintf(B);
-    }
-    C.partition(size, N, rank);
-
-/*         
-    if(rank == 0)
-        master<int>(A, B, C);
-    else
-        slave<int>(A, B, C);
-  */   
-    printf("starting sends\n\n\n\n");
-    // Scatter B to processes
-    err = MPI_Scatter(B.mat, size2, MPI_INT, &(B.mat[rank*N]), size2, MPI_INT, 0, MPI_COMM_WORLD);
-    CHECKMPI(err); 
-    
-    // Scatter A to processes
-    err = MPI_Scatter(A.mat, size2, MPI_INT, &(A.mat[rank*N]), size2, MPI_INT, 0, MPI_COMM_WORLD);
-    CHECKMPI(err); 
-
-    // Perform work on matrix
-    printf("rank %d doing work\n", rank);
-    work(&(A.mat[rank*N]), &(B.mat[rank*N]), &(C.mat[rank*N]));
-
-    // Gather back result to C
-    err= MPI_Gather(&(C.mat[rank*N]), size2, MPI_INT, C.mat, size2, MPI_INT, 0, MPI_COMM_WORLD);
-    CHECKMPI(err); 
 
     MPI_Barrier(MPI_COMM_WORLD);
-    std::cout<<"C done\n";
-    matprintf(C);
+    for(i=0; i<N/size; ++i)
+    {
+        // Scatter B to processes
+        err = MPI_Scatter(Bp->cmat, size2, MPI_INT, &(B.cmat[rank*size2]), size2, MPI_INT, 0, MPI_COMM_WORLD);
+        CHECKMPI(err); 
+        
+        // Scatter A to processes
+        err = MPI_Scatter(Bp->cmat, size2, MPI_INT, &(A.cmat[rank*size2]), size2, MPI_INT, 0, MPI_COMM_WORLD);
+        CHECKMPI(err); 
 
+        // Perform work on matrix
+        work(&(A.cmat[rank*size2]), &(B.cmat[rank*size2]), &(C.cmat[rank*size2]));
+
+        // Gather back result to C
+        err= MPI_Gather(&(C.cmat[rank*size2]), size2, MPI_INT, Cp->cmat, size2, MPI_INT, 0, MPI_COMM_WORLD);
+        CHECKMPI(err); 
+        
+/*
+        MPI_Barrier(MPI_COMM_WORLD);
+        if(rank == 0)
+        {
+        pmatprintf(A, size, "A");
+        cmatprintf(A);
+        pmatprintf(B, size, "B");
+        cmatprintf(B);
+        printf("Indexing ..\n");
+        pmatprintf(C, size, "C");
+        cmatprintf(C);
+        }
+*/
+        
+        if (i!=(N/size-1))
+        {
+            A.shift(size, N);
+            B.shift(size, N, true);
+        }
+    }
 
     if(rank == 0)
     {
         stopclock = std::chrono::high_resolution_clock::now();
         time_span = std::chrono::duration_cast<std::chrono::duration<double>>(stopclock - startclock);
+
+        std::cout<<" = C\n";
+        pmatprintf(C, size, "C");
+
         printf("\nProgram exec time: %f seconds \n\n",(double)time_span.count());
     }
 
@@ -159,86 +155,24 @@ int main (int argc, char *argv[]) {
 template <typename T>
 void work(T* A, T* B, T* C)
 {
-    //submatprintf(A,size,size,"Awork", rank); std::cout<<" x \n "; submatprintf(B,size,size,"Bwork");
+   // submatprintf(A,size,size,"Awork", rank); std::cout<<"\n x \n "; submatprintf(B,size,size,"Bwork", rank);
+//    std::cout<<"\n";
     int i,j,k;
     for(i=0;i<size;i++)
     {
-     //   printf("row %d\n", i);
+   //     printf("row %d\n", i);
         for(j=0;j<size;j++)
         {
-            C[j+i*size] = 0;
             for(k=0;k<size;k++) 
             {
                 C[j+i*size]+=A[k+i*size]*B[j+k*size];
-       //         printf("\tC(%d,%d) += A(%d,%d)*B(%d,%d)\n",i,j,i,k,k,j);
-         //       printf("\t\t%d += %d*%d\n",C[j+i*size],A[k+i*size],B[j+k*size]);
+ //               printf("\tC(%d,%d) += A(%d,%d)*B(%d,%d)\n",i,j,i,k,k,j);
+  //              printf("\t\t%d += %d*%d\n",C[j+i*size],A[k+i*size],B[j+k*size]);
             }
         }
     }
-    //std::cout<<" = "; submatprintf(C,size,size,"Cwork");
-    //std::cout<<"\n\n";
-}
-
-template <typename T>
-void master(Matrix<T>& A, Matrix<T>& B, Matrix<T>& C)
-{
-    int i,j,k=1;
-    for(i=0; i<size; ++i)
-    {
-        for(j=0; j<size; ++j)
-        {
-            if(i==0 && j==0) continue;
-            printf("sending\n");
-            err = MPI_Send(A.smat[k], size2, MPI_INT, k, ATAG, MPI_COMM_WORLD);
-            CHECKMPI(err); 
-            err = MPI_Send(B.smat[k], size2, MPI_INT, k, BTAG, MPI_COMM_WORLD);
-            CHECKMPI(err); 
-            k++;
-        }
-    }
-    
-    work(A.smat[0], B.smat[0], C.smat[0]);
-    k=1;
-    for(i=0; i<size; ++i)
-    {
-        for(j=0; j<size; ++i)
-        {
-            if(i==0 && j==0) continue;
-            std::cout<<"waiting...\n";
-            err = MPI_Recv(C.smat[k], size2, MPI_INT, k, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
-            std::cout<<"recieved\n";
-            CHECKMPI(err); 
-            err = MPI_Send(0, 0, MPI_INT, k, DIETAG, MPI_COMM_WORLD);
-            CHECKMPI(err); 
-            k++;
-        }
-    }
-    std::cout<<"C done\n";
-    matprintf(C);
-
-    return;
-}
-
-template <typename T>
-void slave(Matrix<T>& A, Matrix<T>& B, Matrix<T>& C)
-{
-    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-    
-    printf("Slave rank %d \n",rank);
-    err = MPI_Recv(A.smat[rank], size2, MPI_INT, 0, ATAG, MPI_COMM_WORLD, &status);
-    submatprintf(A.smat[rank], size, size, "Aslave", rank);
-    CHECKMPI(err); 
-
-    err = MPI_Recv(B.smat[rank], size2, MPI_INT, 0, BTAG, MPI_COMM_WORLD, &status);
-    submatprintf(B.smat[rank], size, size, "Bslave", rank);
-    CHECKMPI(err); 
-    
-    if(status.MPI_TAG == DIETAG) return;
-
-    work(A.smat[rank], B.smat[rank], C.smat[rank]);
-
-    err = MPI_Isend(C.smat[rank], size2, MPI_INT, 0, CTAG, MPI_COMM_WORLD, &request);
-    CHECKMPI(err); 
+  //  std::cout<<" = \n"; submatprintf(C,size,size,"Cwork", rank);
+  //  std::cout<<"\n\n";
 }
 
 
--- matrix/src/matlib.hpp
+++ matrix/src/matlib.hpp
@@ -17,106 +17,155 @@ class Matrix
        rows = r;
        cols = c;
        mat = new T[cols*rows]; 
+       cmat = new T[cols*rows]; 
     }
 
     T& operator()(int i, int j) {return mat[j + i * cols];}
-
+    
     Matrix<T>& operator=(const Matrix<T>& rhs)
     {
         if(mat != NULL)
             delete [] mat;
-        mat = NULL;
+        if(cmat != NULL)
+            delete [] cmat;
         mat = new T[rhs.rows*rhs.cols];
-        for(int i=0; i<rhs.rows; i++)
-            for(int j=0; j< rhs.cols; j++)
-                mat[j + i * cols] = rhs.mat[j + i * cols];
+        cmat = new T[rhs.rows*rhs.cols];
+        for(int i=0; i<rhs.rows*rhs.cols; ++i)
+        {
+            mat[i] = rhs.mat[i];
+            cmat[i] = rhs.cmat[i];
+        }
         return *this;
     }
-    
-    //T* submat(int row)
-    //{
-     //   return &this->mat[row*cols];
-    //}
 
-    void partition(int size, int N, int rank, bool transpose = false)
+    void shift(int size, int N, bool colshift = false, bool align = false)
     {
         // size : size of the sub matrix
         // N : number of sub matrices per row or col
         // transpose : transpose the positions of submatrices
+        
+        int row, col, i, r1, r2;
+        T* temp = new T[rows*cols];
+        int bmSize = N/size;
+        int size2 = size*size;
+        int bmSize2 = bmSize*bmSize;
 
-        int row, col, i, j, map=0;
-        if (transpose)
-            for(row=0; row<N; row+=size)
-                for(col=row+size; col<N; col+=size)
-                    for(i=0; i<size; ++i)
-                        for (j=0; j<size; ++j)
+        for(row=0; row<bmSize; ++row)
+            for(col=0; col<size2; ++col)
+                for(i=0; i<bmSize; ++i)
+                {
+                    if(colshift)
+                    {
+                        r1 = i*bmSize+row;
+                        if(align)
+                            r2 = ((i+row)*bmSize)%bmSize2+row;
+                        else
+                            r2 = ((i+1)*bmSize)%bmSize2+row;
+                    }
+                    else
+                    {
+                        // if !align
+                        // row = 0 0 0 1 1 1 2 2 2
+                        // i   = 0 1 2 0 1 2 0 1 2
+                        // r1  = 0 3 6 1 4 7 2 5 8
+                        // r2  = 3 6 0 4 7 1 5 8 2
+                        // else
+                        // row = 0 0 0 1 1 1 2 2 2
+                        // i   = 0 1 2 0 1 2 0 1 2
+                        // r1  = 0 3 6 1 4 7 2 5 8
+                        // r2  = 0 3 6 4 7 1 8 2 5
+                        r1 = row*bmSize+i;
+                        if(align)
+                            r2 = row*bmSize+(i+row)%bmSize;
+                        else
+                            r2 = row*bmSize+(i+1)%bmSize;
+                    }
+                    temp[r1*size2+col] = cmat[r2*size2+col];
+                }
+        
+        delete [] cmat;
+        cmat = temp;
+        
+        /*
+        for(row=0; row<N; row+=size)
+            for(col=0; col<N; col+=size)
+                for(i=0; i<size; ++i)
+                    for (j=0; j<size; ++j)
+                    {
+                        int r1 = row+i; // original row index
+                        int r2 = row+i; // shifted row index
+                        int c1 = col+j; // original col index
+                        int c2 = col+j; // shifted col index
+                        if(colshift)
                         {
-                            int r1 = row+i; // original row index
-                            int r2 = col+i; // transposed row index
-                            int c1 = col+j; // original col index
-                            int c2 = row+j; // transposed col index
-
-                            // compute matrix indices
-                            int idx1 = r1*N+c1;
-                            int idx2 = r2*N+c2;
-
-                            // swap indicies
-                            T temp = mat[idx1];
-                            mat[idx1] = mat[idx2];
-                            mat[idx2] = temp;
+                            cshift = align ? col : size;
+                            r2 = (r2 - cshift) < 0 ? N + (r2 - cshift) : r2 - cshift;
+                        }
+                        else
+                        {
+                            rshift = align ? row : size;
+                            c2 = (c2 - rshift) < 0 ? N + (c2 - rshift) : c2 - rshift;
                         }
 
+                        // compute matrix indices
+                        int idx1 = r1*N+c1;
+                        int idx2 = r2*N+c2;
+
+                        temp[idx2] = mat[idx1];
+                    }
+        */
+ 
+        //setRowIndexing(size, N);
+   }
+
+    void setRowIndexing(int size, int N, bool mapBack=false)
+    {
+        int row, col, i, j, map=0;
+
         // reorganize mat
-        cmat = new T[row*cols];
         for (row=0; row<N; row+=size)   // submatrix "row"
             for (col=0; col<N; col+=size) // submatrix "col"
                 for (i=row; i<row+size; ++i) // full matrix row
                     for (j=col; j<col+size; ++j) // full matrix col
-                        cmat[map++] = mat[i*N+j];
+                    {
+                        if (mapBack)
+                            cmat[map++] = mat[i*N+j];
+                        else
+                            mat[i*N+j] = cmat[map++];
+                    }
     }
 
     void clear()
     {
         int i;
         for(i=0;i<rows*cols;i++)
+        {
             mat[i] = 0;
+            if(cmat != NULL)
+                cmat[i] = 0;
+        }
              
     }
     
 };
 
 template <typename T>
-void matmult(Matrix<T> A, Matrix<T>B, Matrix<T> C)
+void pmatprintf(Matrix<T> A, int size, std::string s = "")
 {
-    int i,j,k;
-    for(i=0;i<A.rows;i++)
-        for(j=0;j<B.cols;j++)
-            for(k=0;k<A.cols;k++) 
-                C(i,j)+=A(i,k)*B(k,j);
-}
-
-template <typename T>
-void matprintf(Matrix<T> A){
+    A.setRowIndexing(size, A.rows, false);
 	int i,j,m,n;
-    m = A.rows;
-    n = A.cols;
+    m = A.rows < 15 ? A.rows : 15;
+    n = A.cols < 15 ? A.cols : 15;
 	for(i=0;i<m;i++)
     {
-        printf("\t\t");
+        if(i%size==0) printf("\t\t%*s\n", size*size, "-");
+        printf("%s\t\t", s.c_str());
         for(j=0;j<n;j++)    
+        {
+            if(j%size==0) printf(" | ");
             printf("%d ", A(i,j));
+        }
         printf("\n");
     }
 }
 
-template <typename T>
-void submatprintf(T* A, int rows, int cols, std::string s = "", int rank = 0){
-	int i,j;
-	for(i=0;i<rows;i++)
-    {
-        printf("submat in %d %s\t ", rank, s.c_str());
-        for(j=0;j<cols;j++)    
-            printf("%d ", A[i * cols + j]);
-        printf("\n");
-    }
-}
