4d495d5de32874ca2e21f5fe44225b33b7fa3da8
thenriod-ubuntu-vm <thenriod@ubuntu.(none)>
2014-04-09 12:41:27 -0700

    Wrote a function that reads a universe of bodies from a simple ASCII file.

--- nbody/src/nbody.cpp
+++ nbody/src/nbody.cpp
@@ -21,6 +21,46 @@ struct Force
     float y;
 };
 
+
+void readNbodyData( char* file_name, Body**& universe );
+
+void readNbodyData( char* file_name, Body**& universe )
+{
+  // variables
+  FILE* file = NULL;
+  int i = 0;
+
+  // open the file
+  file = fopen( file_name );
+
+  // read in the number of bodies
+  fscanf( file, "%d", &NBODIES );
+
+  // allocate the array to contain pointers to all of the bodies
+  universe = calloc( NBODIES, sizeof( Body* ) );
+  for( i = 0; i < NBODIES; i++ )
+  {
+    // allocate the bodies themselves
+    universe[i] = calloc( 1, sizeof( Body ) );
+  }
+
+  // read in all body information
+  for( i = 0; i < NBODIES; ++i )
+  {
+    // read in the mass value
+    fscanf( file, "%f", &(universe[i]->mass) );
+
+    // read in the body's position as a spacial coordinate triple
+    fscanf( file, "%f %f %f", &(universe[i]->position[0]),
+            &(universe[i]->position[1]), &(universe[i]-position[2]) );
+  }
+
+  // return the universe array by reference
+  // (note: we are not error checking for production efficiency)
+}
+
+
+
 int main(int* argv, char** argc)
 {
     Body *BODIES = new Body[argv[0]];
