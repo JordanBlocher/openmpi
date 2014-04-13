c3ba0527cf3be9e0c4cd0bd7469e4ab9e8bb2eeb
frags <frags@mobileZeta.(none)>
2014-04-13 10:49:59 -0700

    Makefile and init update

--- nbody/init.py
+++ nbody/init.py
@@ -1,8 +1,8 @@
 #!bin/python
-from random import seed, randint, uniform
+from random import seed, randint, uniform, random
 
 seed(0)
 
 print "20"
 for i in range(0,20):
-    print uniform(1, 200), " ", uniform(1, 200), " ", uniform(1, 200), randint(1,5)
+    print uniform(1, 200), " ", uniform(1, 200), " ", uniform(1, 200), " ", random(), " ", random(), " ", random(), " ", randint(1,5)
